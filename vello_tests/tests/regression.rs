415cbe134b2b4117fe5400537b163b2c4442d89b
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-01-08 14:13:58 +0000

    Add a test for zero width strokes

--- vello_tests/tests/regression.rs
+++ vello_tests/tests/regression.rs
@@ -4,7 +4,7 @@
 //! Tests to ensure that certain issues which don't deserve a test scene don't regress
 
 use vello::{
-    kurbo::{Affine, RoundedRect, Stroke},
+    kurbo::{Affine, Rect, RoundedRect, Stroke},
     peniko::color::palette,
     AaConfig, Scene,
 };
@@ -20,6 +20,23 @@ fn rounded_rectangle_watertight() {
     scene.stroke(&stroke, Affine::IDENTITY, palette::css::WHITE, None, &rect);
     let mut params = TestParams::new("rounded_rectangle_watertight", 70, 30);
     params.anti_aliasing = AaConfig::Msaa16;
+    snapshot_test_sync(scene, &params)
+        .unwrap()
+        .assert_mean_less_than(0.001);
+}
+
+/// Test created from <https://github.com/linebender/vello/issues/662>
+#[test]
+#[cfg_attr(skip_gpu_tests, ignore)]
+fn stroke_width_zero() {
+    let mut scene = Scene::new();
+    let stroke = Stroke::new(0.0);
+    let rect = Rect::new(10.0, 10.0, 40.0, 40.0);
+    let rect_stroke_color = palette::css::PEACH_PUFF;
+    scene.stroke(&stroke, Affine::IDENTITY, rect_stroke_color, None, &rect);
+    let mut params = TestParams::new("stroke_width_zero", 50, 50);
+    params.anti_aliasing = AaConfig::Msaa16;
+    // params.use_cpu = true;
     snapshot_test_sync(scene, &params)
         .unwrap()
         .assert_mean_less_than(0.001);
